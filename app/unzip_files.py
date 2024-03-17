9d3421d895208cf4fb2fbeaad8ac5aa086c0fadf
Abrar Shahriar <abetrack3@yahoo.com>
2024-03-17 13:31:18 +0600

    parallelized archive extraction

--- app/unzip_files.py
+++ app/unzip_files.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import zipfile
 import patoolib
+from concurrent.futures import ThreadPoolExecutor
 
 def extract_zip_file(zip_file, extract_path):
     with zipfile.ZipFile(zip_file, 'r') as zip_ref:
@@ -15,27 +16,25 @@ def extract_zip_and_rar_files(input_dir, output_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    for filename in os.listdir(input_dir):
-        file_path = os.path.join(input_dir, filename)
-        file_name_without_extension: str = os.path.splitext(filename)[0]
-        destination_file_path: str = os.path.join(output_dir, file_name_without_extension)
+    with ThreadPoolExecutor() as executor:
 
-        if filename.endswith('.zip'):
-            extract_zip_file(file_path, destination_file_path)
-            print(f'Extracted: {filename} (ZIP)')
+        for filename in os.listdir(input_dir):
+            file_path = os.path.join(input_dir, filename)
+            file_name_without_extension: str = os.path.splitext(filename)[0]
+            destination_file_path: str = os.path.join(output_dir, file_name_without_extension)
 
-        elif filename.endswith('.rar'):
-            extract_rar_file(file_path, destination_file_path)
-            print(f'Extracted: {filename} (RAR)')
+            if filename.endswith('.zip'):
+                executor.submit(extract_zip_file, file_path, destination_file_path)
+                print(f'Extracted: {filename} (ZIP)')
 
-        elif filename.endswith('.ipynb') or filename.endswith('.py'):
-            os.makedirs(destination_file_path)
-            shutil.copy(file_path, destination_file_path)
-            print(f'Copied: {filename}')
+            elif filename.endswith('.rar'):
+                executor.submit(extract_rar_file, file_path, destination_file_path)
+                print(f'Extracted: {filename} (RAR)')
 
+            elif filename.endswith('.ipynb') or filename.endswith('.py'):
+                os.makedirs(destination_file_path)
+                shutil.copy(file_path, destination_file_path)
+                print(f'Copied: {filename}')
 
-if __name__ == '__main__':
-    input_directory = 'input_directory_path'  # Replace with the path to your input directory
-    output_directory = 'output_directory_path'  # Replace with the path to your output directory
+    executor.shutdown(wait=True)
 
-    extract_zip_and_rar_files(input_directory, output_directory)
