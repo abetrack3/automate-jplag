a6611f93f05c735ad6b6f43ef73d7e4754accb47
Abrar Shahriar <abetrack3@yahoo.com>
2024-03-17 10:17:22 +0600

    excel report sorted - student id first, then similarity percentage

--- app/excel_report_generator.py
+++ app/excel_report_generator.py
@@ -21,6 +21,17 @@ def __extract_student_id_from_file_name(file_name: str) -> Optional[str]:
         return None
 
 
+def __custom_sort_key_function(element: Tuple[str, str, str, str, float,]) -> Tuple[str, float, str, str, str,]:
+
+    return (
+        element[0] if element[0] is not None else 'z',
+        1.0 - (element[4] if element[4] is not None else 0.0),
+        element[1] if element[1] is not None else 'z',
+        element[2] if element[2] is not None else 'z',
+        element[3] if element[3] is not None else 'z',
+    )
+
+
 def generate_excel_report() -> None:
 
     # Extract result zip file
@@ -64,7 +75,7 @@ def generate_excel_report() -> None:
                                    each_comparison[1],
                                    similarity_in_percentage))
 
-    result_set = sorted(result_set)
+    result_set.sort(key = __custom_sort_key_function)
 
     source_student_ids, target_student_ids, source_student_file_name, target_student_file_name, similarities = zip(*result_set)
 
