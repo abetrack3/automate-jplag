c1b548b55eb462eb3a1e2ecb7579202a721b56a9
BRACITS\abrar.shahriar <abrar.shahriar@bracits.com>
2025-07-11 19:28:20 +0600

    updated cleanup policy

--- app/cleanup.py
+++ app/cleanup.py
@@ -1,46 +1,40 @@
-import os, shutil
+import os
+import shutil
 from typing import List
 
 
 def remove_generated_artifacts(exclude_result_artifacts: bool = False) -> None:
-
     print('\n\nRemoving artifacts')
 
-    removable_directories: List[str] = filter(
-        lambda sub_folder: sub_folder.endswith(
-            ('UNZIPPED',
-            'CONVERTED',
-            'JPLAG SCANNABLE',
-            'result'),
-            ),
-        os.listdir(),
-    )
+    excluded_directories = ['results'] if exclude_result_artifacts else []
+
+    removable_directories: List[str] = [
+        sub_folder for sub_folder in os.listdir()
+        if sub_folder.endswith(('UNZIPPED', 'CONVERTED', 'JPLAG SCANNABLE', 'results')) and
+        sub_folder not in excluded_directories
+    ]
 
     for removable_directory in removable_directories:
-        
+
         try:
-        
+
             shutil.rmtree(removable_directory)
 
             print(f'Removed: {removable_directory}')
-        
-        except Exception:
-        
+
+        except OSError:
+
             pass
 
-    if exclude_result_artifacts is True:
+    removable_files: List[str] = ['results.jplag']
+    excluded_files = ['result.xlsx']
 
-        return    
-
-    removable_files: List[str] = ['result.zip', 'result.xlsx']
+    if exclude_result_artifacts is not True:
+        removable_files += excluded_files
 
     for removable_file in removable_files:
 
         if os.path.exists(removable_file):
-
             os.remove(removable_file)
 
             print(f'Removed: {removable_file}')
-            
-
-    
