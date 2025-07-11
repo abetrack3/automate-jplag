3634ea15350f548ac3000ea48b1a9f80357a09b3
BRACITS\abrar.shahriar <abrar.shahriar@bracits.com>
2025-07-11 22:20:23 +0600

    reformat file

--- app/unzip_files.py
+++ app/unzip_files.py
@@ -4,15 +4,18 @@ import zipfile
 import patoolib
 from concurrent.futures import ThreadPoolExecutor
 
+
 def extract_zip_file(zip_file, extract_path):
     with zipfile.ZipFile(zip_file, 'r') as zip_ref:
         zip_ref.extractall(extract_path)
     print(f'\nExtracted: {zip_file} (ZIP)\n')
 
+
 def extract_rar_file(rar_file, extract_path):
     os.makedirs(extract_path)
     patoolib.extract_archive(rar_file, outdir=extract_path)
     print(f'\nExtracted: {rar_file} (RAR)\n')
+
 
 def extract_zip_and_rar_files(input_dir, output_dir):
     if not os.path.exists(output_dir):
@@ -37,4 +40,3 @@ def extract_zip_and_rar_files(input_dir, output_dir):
                 print(f'Copied: {filename}')
 
     executor.shutdown(wait=True)
-
