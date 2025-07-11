db8953abf67968929db5ec4a6ffa3e4ba80031c8
BRACITS\abrar.shahriar <abrar.shahriar@bracits.com>
2025-07-11 22:21:18 +0600

    updated cleanup policy once again

--- app/cleanup.py
+++ app/cleanup.py
@@ -6,12 +6,9 @@ from typing import List
 def remove_generated_artifacts(exclude_result_artifacts: bool = False) -> None:
     print('\n\nRemoving artifacts')
 
-    excluded_directories = ['results'] if exclude_result_artifacts else []
-
     removable_directories: List[str] = [
         sub_folder for sub_folder in os.listdir()
-        if sub_folder.endswith(('UNZIPPED', 'CONVERTED', 'JPLAG SCANNABLE', 'results')) and
-        sub_folder not in excluded_directories
+        if sub_folder.endswith(('UNZIPPED', 'CONVERTED', 'JPLAG SCANNABLE', 'results'))
     ]
 
     for removable_directory in removable_directories:
@@ -26,11 +23,10 @@ def remove_generated_artifacts(exclude_result_artifacts: bool = False) -> None:
 
             pass
 
-    removable_files: List[str] = ['results.jplag']
-    excluded_files = ['result.xlsx']
+    if exclude_result_artifacts:
+        return
 
-    if exclude_result_artifacts is not True:
-        removable_files += excluded_files
+    removable_files: List[str] = ['results.jplag', 'result.xlsx']
 
     for removable_file in removable_files:
 
